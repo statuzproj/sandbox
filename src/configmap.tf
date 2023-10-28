d41eac092d4f09ff7b6a433d767b9dfe06c37649
Timam <mail@timam.io>
2023-10-28 14:47:12 +0600

    refactor confgimap

--- src/configmap.tf
+++ src/configmap.tf
@@ -7,15 +7,27 @@ resource "kubernetes_config_map_v1" "watchpoints" {
   data = {
     "endpoints.yaml" = <<EOF
 - name: google_bd
-  type: webpage
-  target: https://www.google.com.bd/
+  target:
+    type: webpage
+    endpoint: https://www.google.com.bd/
+    interval: 15
 - name: grafana_web
-  type: api
-  method: GET
-  target: https://grafana.com/healthz
+  target:
+    type: api
+    endpoint: https://grafana.com/healthz
+    interval: 60
+    method: GET
+- name: search_a_cat
+  target:
+    type: api
+    endpoint: https://api.thecatapi.com/v1/images/search
+    interval: 60
+    method: GET
 - name: cloudflare_dns
-  type: ping
-  target: 1.1.1.1
+  target:
+    type: ip
+    interval: 15
+    endpoint: 1.1.1.1
   EOF
   }
 }

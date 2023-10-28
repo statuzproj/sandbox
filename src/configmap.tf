d0bd4a2910b66bd0e5eaa1ae8be5d15b1ad5ba8d
Timam <mail@timam.io>
2023-10-29 01:45:51 +0600

    only webpage is supported atm

--- src/configmap.tf
+++ src/configmap.tf
@@ -11,23 +11,29 @@ resource "kubernetes_config_map_v1" "watchpoints" {
     type: webpage
     endpoint: https://www.google.com.bd/
     interval: 15
-- name: grafana_web
+- name: google_in
   target:
-    type: api
-    endpoint: https://grafana.com/healthz
-    interval: 60
-    method: GET
-- name: search_a_cat
-  target:
-    type: api
-    endpoint: https://api.thecatapi.com/v1/images/search
-    interval: 60
-    method: GET
-- name: cloudflare_dns
-  target:
-    type: ip
+    type: webpage
+    endpoint: https://www.google.co.in/
     interval: 15
-    endpoint: 1.1.1.1
   EOF
   }
 }
+
+#- name: grafana_web
+#  target:
+#    type: api
+#    endpoint: https://grafana.com/healthz
+#    interval: 60
+#    method: GET
+#- name: search_a_cat
+#  target:
+#    type: api
+#    endpoint: https://api.thecatapi.com/v1/images/search
+#    interval: 60
+#    method: GET
+#- name: cloudflare_dns
+#  target:
+#    type: ip
+#    interval: 15
+#    endpoint: 1.1.1.1
